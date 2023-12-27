00072499ec3394449695a92fd9de276d3909d620
Tomek20225 <tomek202255@gmail.com>
2023-12-27 01:09:17 +0100

    Refactored Day 1 Python solutions

--- day-1/python/part-1.py
+++ day-1/python/part-1.py
@@ -1,18 +1,39 @@
-FILE = open('../input.txt', "r")
+def calculate_sum_from_file(file_path: str) -> int:
+    """
+    Parses a file for numbers (only digits) and returns their sum.
+    Each line in the file is expected to contain at least one numeric character.
+    Lines can contain other characters.
+    
+    Args:
+    file_path (str): The path to the file to be read.
+    
+    Returns:
+    int: The sum of the processed numbers.
+    """
 
-sum = 0
+    total_sum = 0
 
-for line in FILE.readlines():
-    nums: list[int] = []
+    try:
+        with open(file_path, "r") as file:
+            for line in file:
+                nums = [int(char) for char in line if char.isnumeric()]
+                if nums:
+                    first, last = nums[0], nums[-1]
+                    val = (first * 10) + last
+                    total_sum += val
+                else:
+                    print(f"No numeric characters found in line: {line}")
 
-    for char in line:
-        if char.isnumeric():
-            nums.append(int(char))
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return 0
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
 
-    first = nums[0]
-    last = nums[len(nums) - 1]
-    val = (first * 10) + last
+    return total_sum
 
-    sum += val
-
-print(sum)
+# Solution
+file_path = '../input.txt'
+result = calculate_sum_from_file(file_path)
+print(result)
--- day-1/python/part-2.py
+++ day-1/python/part-2.py
@@ -1,27 +1,53 @@
-FILE = open('../input.txt', "r")
-SPELLED_NUMS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+def calculate_sum_from_file(file_path: str) -> int:
+    """
+    Parses a file for numbers (both digits and spelled out) and returns their sum.
+    Each line in the file is expected to contain at least one number in either format.
+    Lines can contain other characters.
+    
+    Args:
+    file_path (str): The path to the file to be read.
+    
+    Returns:
+    int: The sum of the processed numbers.
+    """
+        
+    SPELLED_TO_NUM = {word: i + 1 for i, word in enumerate([
+        'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+    ])}
+    total_sum = 0
 
-sum = 0
+    try:
+        with open(file_path, 'r') as FILE:
+            for line in FILE:
+                nums: list[int] = []
 
-for line in FILE.readlines():
-    nums: list[int] = []
+                for i, char in enumerate(line):
+                    if char.isnumeric():
+                        nums.append(int(char))
+                    else:
+                        for spelled_num, num in SPELLED_TO_NUM.items():
+                            if char == spelled_num[0] and line.startswith(spelled_num, i):
+                                nums.append(num)
+                                break
+                
+                if len(nums) >= 1:
+                    first, last = nums[0], nums[-1]
+                    val = (first * 10) + last
+                    total_sum += val
+                else:
+                    print(f"No numeric characters and no spelled out words found in line: {line}")
+                
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return 0
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
+    
+    return total_sum
 
-    for i, char in enumerate(line):
-        if char.isnumeric():
-            nums.append(int(char))
-            continue
 
-        for num, spelled_num in enumerate(SPELLED_NUMS):
-            if char == spelled_num[0]:
-                substr = line[i: i + len(spelled_num)]
-                if substr == spelled_num:
-                    nums.append(num + 1)
-                    break
-
-    first = nums[0]
-    last = nums[len(nums) - 1]
-    val = (first * 10) + last
-
-    sum += val
-
-print(sum)
+# Solution
+file_path = '../input.txt'
+result = calculate_sum_from_file(file_path)
+print(result)
